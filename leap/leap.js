2fd5a2eb507d8a44a0ce82a82d22503699082574
Dianna <hello@thisisdianna.com>
2018-11-14 21:34:11 -0500

    updated answer for leap

--- leap/leap.js
+++ leap/leap.js
@@ -1,9 +1,6 @@
 export const isLeap = (year) => {
-  if (year % 4 !== 0) {
-    return false;
-  } if (year % 4 === 0 && year % 100 !== 0) {
-    return true;
-  } if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+  // if the year is divisible by 4 but not divisible by 100 OR it's divisible by 400 (which is divisible by 4, it is a leap year)
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
     return true;
   }
 };
